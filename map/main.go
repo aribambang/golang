93ffaa37fb27ad391d88840f38db8607e7549ba3
Ari Bambang Kurniawan <aribk210@gmail.com>
2021-03-23 13:12:21 +0700

    add map

---
+++ map/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	// var colors map[string]string
+	// colors := make(map[string]string)
+
+	// colors["white"] = "#fffff"
+
+	// colors := make(map[int]string)
+
+	// colors[10] = "#ffffff"
+	// delete(colors, 10)
+
+	// fmt.Println(colors)
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
